70868f4cf05bc029129a4fc0422dc05c5392a0a9
Julio Rodriguez <jircdev@gmail.com>
2024-12-30 11:19:40 -0300

    refactor: Update imports of ipc package

--- service/fork.js
+++ service/fork.js
@@ -1,28 +1,28 @@
 process.title = 'BeyondJS files watchers monitor';
 
-const ipc = require('@beyond-js/ipc');
-const watchers = new (require('./watchers'));
+const ipc = require('@beyond-js/ipc/main');
+const watchers = new (require('./watchers'))();
 
 ipc.handle('create', params => watchers.create(params.container));
 ipc.handle('delete', async params => await watchers.delete(params.id));
 
 ipc.handle('listeners.create', params => {
-    if (!params) throw new Error(`Invalid parameters`);
-    if (!params.watcher) throw new Error(`Watcher parameter not defined`);
-    if (!watchers.has(params.watcher)) throw new Error(`Watcher ${params.watcher} is not registered`);
+	if (!params) throw new Error(`Invalid parameters`);
+	if (!params.watcher) throw new Error(`Watcher parameter not defined`);
+	if (!watchers.has(params.watcher)) throw new Error(`Watcher ${params.watcher} is not registered`);
 
-    const watcher = watchers.get(params.watcher);
-    const listener = watcher.listeners.create(params.watcher, params.path, params.filter);
+	const watcher = watchers.get(params.watcher);
+	const listener = watcher.listeners.create(params.watcher, params.path, params.filter);
 
-    return listener.id;
+	return listener.id;
 });
 
 ipc.handle('listeners.delete', params => {
-    if (!params) throw new Error(`Invalid parameters`);
-    if (!params.watcher) throw new Error(`Watcher parameter not defined`);
-    if (!params.id) throw new Error(`Listener parameter not defined`);
-    if (!watchers.has(params.watcher)) throw new Error(`Watcher ${params.watcher} is not registered`);
+	if (!params) throw new Error(`Invalid parameters`);
+	if (!params.watcher) throw new Error(`Watcher parameter not defined`);
+	if (!params.id) throw new Error(`Listener parameter not defined`);
+	if (!watchers.has(params.watcher)) throw new Error(`Watcher ${params.watcher} is not registered`);
 
-    const watcher = watchers.get(params.watcher);
-    watcher.listeners.stop(params.id);
+	const watcher = watchers.get(params.watcher);
+	watcher.listeners.stop(params.id);
 });
